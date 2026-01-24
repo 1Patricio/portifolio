9a664a74010c435a0ee6e21cc165d2c281c38fa8
Anderson Patricio <andersongpatricio@hotmail.com>
2026-01-24 10:28:31 -0300

    feat: adicionando projeto 4

--- src/components/ProjectListComponent.vue
+++ src/components/ProjectListComponent.vue
@@ -110,7 +110,7 @@
   <!-- Projeto 2 - Rick and Morty -->
   <div class="hero mt-2">
     <div class="hero-content flex-col lg:flex-row">
-      <video width="640" height="360" controls muted autoplay loop class="lg:mr-4 rounded">
+      <video width="640" height="360" controls muted autoplay loop class="lg:mr-4 rounded border border-black">
         <source src="../assets/videos/project2.mp4" type="video/mp4" />
         O seu navegador não suporta a tag de vídeo
       </video>
@@ -222,6 +222,35 @@
           <div class="badge badge-outline">Liderança</div>
           <div class="badge badge-outline">Comunicação</div>
           <div class="badge badge-outline">Organização</div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Projeto 4 - Cards Marketplace -->
+  <div class="hero mt-2">
+    <div class="hero-content flex-col lg:flex-row">
+      <video width="640" height="360" controls muted autoplay loop class="lg:mr-4 rounded border border-black">
+        <source src="../assets/videos/project4.mp4" type="video/mp4" />
+        O seu navegador não suporta a tag de vídeo
+      </video>
+      <div>
+        <h1 class="text-3xl font-bold sm:text-5xl">Cards Marketplace!</h1>
+        <p class="py-6">
+          Desenvolvi uma aplicação completa em <b>Vue.js</b> para negociação e troca de cartas colecionáveis (Cards Marketplace). Utilizei arquitetura componentizada (SFCs), roteamento organizado com guardas de autenticação, layouts reutilizáveis e <b>Quasar Framework</b> para uma interface responsiva.
+          O projeto segue boas práticas de desenvolvimento moderno, incluindo tipagem estática com <b>TypeScript</b>, gerenciamento de estado reativo via <b>Pinia</b> e carregamento otimizado (uso de Virtual Scroll)
+        </p>
+        <button class="btn bg-black text-white border-black mx-1 mb-1">
+          <svg aria-label="GitHub logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"></path></svg>
+          <a href="https://github.com/1Patricio/cards-marketplace" target="_blank">Repositório</a>
+        </button>
+
+        <div class="card-actions justify-start mt-2 md:justify-end md:mt-0">
+          <div class="badge badge-outline">Vue</div>
+          <div class="badge badge-outline">Quasar</div>
+          <div class="badge badge-outline">Pinia</div>
+          <div class="badge badge-outline">TypeScript</div>
+          <div class="badge badge-outline">Vite</div>
         </div>
       </div>
     </div>
